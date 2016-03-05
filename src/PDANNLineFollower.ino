10acc7f9ccab17acb11208b489113caf65416c27
Manash Mandal <manashmndl@gmail.com>
2016-03-05 13:21:34 +0600

    Update PDANNLineFollower.ino

--- src/PDANNLineFollower.ino
+++ src/PDANNLineFollower.ino
@@ -5,39 +5,29 @@
 const u_int _speed = 100;
 
 //Sensor pins
-vector <u_int> s{0, 1, 2, 3, 4, 5, 6, 7};
-vector <u_int> s_6 {6, 5, 4, 3, 2, 1};
 vector <u_int> qtr{0, 1, 2, 3, 4, 5, 6, 7};
-// vector <u_int> s_6 {1, 2, 3, 4, 5, 6};
 
 u_int lm[] = {5, 3};
 u_int rm[] = {6, 9};
 
 LineFollower lineFollower(lm, rm, qtr);
 
-
 void setup()
 {
-
   LineFollowingRobot.set_motors_pins(lm, rm);
   LineFollowingRobot.set_sensors_pins(qtr);
   LineFollowingRobot.init();
-
   lineFollower.init();
   LineFollowingRobot.set_speed(75);
   weights();
 }
 
-
 #if defined(NEURAL_NETWORK)
 void loop()
 {
   weights();
-  // printNeuralNetworkOutputViaBluetooth();
-  // LineFollowingRobot.wireless_debug();
   neural_network_drive();
 }
-
 
 #else
 void loop()
